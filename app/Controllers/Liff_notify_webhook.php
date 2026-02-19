0bb51b6ada8a41be5021ca826ff2d998275c18d8
trynotcatcherror <garinsookkalya@gmail.com>
2026-02-19 16:48:10 +0700

    test

--- app/Controllers/Liff_notify_webhook.php
+++ app/Controllers/Liff_notify_webhook.php
@@ -7,7 +7,7 @@ use CodeIgniter\Controller;
 class Liff_notify_webhook extends Controller {
 
     public function webhook() {
-        helper('general');
+        helper(['general', 'date_time']);
         $this->response->setContentType('text/plain');
         $debug = [
             'timestamp' => date('c'),
